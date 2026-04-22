d277065f612fc71f4395505e54ecc0528c1e9189
Jonatas <jonatasdavi.ads@gmail.com>
2026-04-22 17:07:56 -0300

    criando o método Adicionar.

--- estrutura_dados.py
+++ estrutura_dados.py
@@ -3,4 +3,18 @@ class Nodo:
         self.valor = valor 
         self.proximo = None
 
-        
+
+class Lista: 
+    def __init__(self):
+        self.cabeca = None
+
+    def adicionar(self, valor):
+        novo_nodo = Nodo(valor)
+        if not self.cabeca:
+            self.cabeca = novo_nodo
+
+        else:
+            atual = self.cabeca
+            while atual.proximo:
+                atual = atual.proximo
+            atual.proximo = novo_nodo    
